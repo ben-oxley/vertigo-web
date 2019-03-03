9ca36c3af2a3aa21fa682a0664a265bb9e1a0064
Ben Oxley <ben.oxley@gmail.com>
2019-03-03 20:23:18 +0000

    Adding updates to support pwa install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'ngx-app',
@@ -12,10 +13,21 @@ import { AnalyticsService } from './@core/utils/analytics.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService) {
+  constructor(private analytics: AnalyticsService, private swUpdate: SwUpdate) {
   }
 
   ngOnInit() {
     this.analytics.trackPageViews();
+
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+        if (confirm("New version available. Load New Version?")) {
+
+          window.location.reload();
+        }
+      });
+    }
   }
 }
