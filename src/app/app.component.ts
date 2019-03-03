2276b1949e1c87659fb976da13a7cd2addc0aa74
Ben Oxley <ben.oxley@gmail.com>
2019-03-03 20:46:30 +0000

    Now working as a fully offline installable application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SwUpdate } from '@angular/service-worker';
 
@@ -13,7 +13,37 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class AppComponent implements OnInit {
 
+  deferredPrompt: any;
+  showButton = false;
+
   constructor(private analytics: AnalyticsService, private swUpdate: SwUpdate) {
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
   }
 
   ngOnInit() {
