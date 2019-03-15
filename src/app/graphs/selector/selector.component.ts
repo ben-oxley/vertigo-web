b65205fee45effa94593520e530ae2dd47c82fc0
Ben Oxley <ben.oxley@gmail.com>
2019-03-15 20:11:04 +0000

    Progress on data series selectors

--- src/app/graphs/selector/selector.component.ts
+++ src/app/graphs/selector/selector.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges, Input, Output } from '@angular/core';
 import { Dataspec } from '../../@processing/dataspec';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 
@@ -13,9 +14,21 @@ export class SelectorComponent implements OnInit {
   constructor() { }
 
   public Dataspec:Dataspec = new Dataspec();
-  public SelectedValue:any = {columns:[]}
+  @Output() SelectedValue:any = {columns:[]}
 
   ngOnInit() {
+  }
+  
+  ngOnChanges(changes: SimpleChanges){
+    console.log(this.SelectedValue);
+  }
+
+  public selectionChanged(event:any){
+    console.log(Object.values(event).map(v=>{
+      let key:any = (<any>v).value.split(":")[1].trim().replace('\'','').replace('\'','').split(",");
+      let group:any = this.Dataspec.Types.find(t=>t.id===key[0]);
+      return group.columns.find(d=>d.name===key[1]);
+    }));
   }
 
 }
