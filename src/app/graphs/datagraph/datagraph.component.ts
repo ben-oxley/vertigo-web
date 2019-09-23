17bc93ebf9d7a0cbf544a0c799a58b5215dc0e4c
Ben Oxley <ben.oxley@gmail.com>
2019-09-23 22:18:19 +0100

    Improving theming and screen use

--- src/app/graphs/datagraph/datagraph.component.ts
+++ src/app/graphs/datagraph/datagraph.component.ts
@@ -10,49 +10,55 @@ import { Data } from '../../processing/data';
 })
 export class DataGraphComponent {
 
-  @Input() InputData:any = [];
+  @Input() InputData: any = [];
 
-  private configOptions:any = {
-    type: 'scatter', 
-    mode: 'lines+points', 
-    marker: {color: 'red'} ,
+  private configOptions: any = {
+    type: 'scatter',
+    mode: 'lines+points',
+    marker: { color: 'red' },
   };
 
-  private seriesColours:string[]=['red','green','blue'];
+  private seriesColours: string[] = ['red', 'green', 'blue'];
 
-  @Output() GraphData:any = [{ 
-    x: Array.from(Array(100).keys()), 
-    y: Array.from(Array(100).keys()).map(a=>Math.sin(a)), 
-    type: 'scatter', 
-    mode: 'lines+points', 
-    marker: {color: 'red'} 
+  @Output() GraphData: any = [{
+    x: Array.from(Array(100).keys()),
+    y: Array.from(Array(100).keys()).map(a => Math.sin(a)),
+    type: 'scatter',
+    mode: 'lines+points',
+    marker: { color: 'red' }
   }];
 
-  public xaxis:any = {
+  public xaxis: any = {
     automargin: true,
   };
-  public yaxis:any = {
+  public yaxis: any = {
     automargin: true,
   };
-  public layout:any = {autosize: true,margin: {
-    l: 50,
-    r: 0,
-    b: 50,
-    t: 0,
-    pad: 4
-  },};
+  public layout: any = {
+    autosize: true,
+    plot_bgcolor: "#FFF3",
+    paper_bgcolor: "#FFF3",
+    font: { color: '#afafaf' },
+    margin: {
+      l: 50,
+      r: 10,
+      b: 50,
+      t: 0,
+      pad: 4
+    },
+  };
 
   ngOnChanges(changes: SimpleChanges) {
     let i = 0;
-    if (changes.InputData.currentValue) this.GraphData = changes.InputData.currentValue.filter(d=>d!=null).map(d=>{
-      if (d){
+    if (changes.InputData.currentValue) this.GraphData = changes.InputData.currentValue.filter(d => d != null).map(d => {
+      if (d) {
         d.type = this.configOptions.type;
-        d.marker = {color:this.seriesColours[i++]}
+        d.marker = { color: this.seriesColours[i++] }
         d.mode = this.configOptions.mode;
       }
       return d;
     });
     else this.GraphData = null;
-    
+
   }
 }
