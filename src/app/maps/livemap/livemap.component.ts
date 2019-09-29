c0375ffd3ad54d5ed5ba4e49c24b5dd4e8a5e901
Ben Oxley <ben.oxley@gmail.com>
2019-09-29 17:38:52 +0100

    Fixed mapping

--- src/app/maps/livemap/livemap.component.ts
+++ src/app/maps/livemap/livemap.component.ts
@@ -13,10 +13,10 @@ export class LivemapComponent implements OnInit, OnChanges {
 
   @Input() lat = 0;
   @Input() lon = 0;
-
+  private map:L.Map;
   public options = {
     layers: [
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '...' }),
     ],
     zoom: 5,
     center: L.latLng({ lat: this.lat, lng: this.lon }),
@@ -28,23 +28,33 @@ export class LivemapComponent implements OnInit, OnChanges {
   public layersControl = {
     baseLayers: {
       'Open Street Map': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      'Open Cycle Map': L.tileLayer('https://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    },
-    overlays: {
-      'Big Circle': L.circle([46.95, -122], { radius: 5000 }),
-      'Big Square': L.polygon([[46.8, -121.55], [46.9, -121.55], [46.9, -121.7], [46.8, -121.7]])
+      'Esri World Imagery': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+      }),
+      'CartoDB DarkMatter': L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+      })
     }
   };
+
+  reload(){
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.lat && this.lon) {
       this.options.center = L.latLng({ lat: this.lat, lng: this.lon })
-      this.layers[0] = L.marker([this.lat, this.lon], {
+      const markerIcon = L.icon({
         iconSize: [25, 41],
         iconAnchor: [13, 41],
         iconUrl: 'assets/img/markers/marker-icon.png',
         shadowUrl: 'assets/img/markers/marker-shadow.png'
       });
+      this.layers[0] = L.marker([this.lat, this.lon], { icon: markerIcon });
     }
   }
 
@@ -52,6 +62,13 @@ export class LivemapComponent implements OnInit, OnChanges {
 
   ngOnInit() {
 
+  }
+
+  onMapReady(map: L.Map) {
+    this.map = map;
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 0);
   }
 
 }
