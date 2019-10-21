baf13def94822088acd83f266d344818cdb4e9fc
Ben Oxley <ben.oxley@gmail.com>
2019-10-21 21:18:04 +0100

    Fixed build errors and updated configuration

--- src/app/processing/processing.module.ts
+++ src/app/processing/processing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Quat2EulData } from './processes/quat2euldata';
 
 @NgModule({
-  declarations: [Quat2EulData],
+  declarations: [],
   imports: [
     CommonModule
   ],
   exports: [
-    Quat2EulData
+    
   ]
 })
 export class ProcessingModule { }
