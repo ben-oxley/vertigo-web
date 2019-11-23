6cc30a40e2baa59255f14980b88a43a24f3e77af
Ben Oxley <ben.oxley@gmail.com>
2019-11-23 01:34:05 +0000

    Updating tutuorials

--- src/app/pages/trampoline/trampoline.component.ts
+++ src/app/pages/trampoline/trampoline.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { VertigoRawData, VertigoProcessedData } from 'src/app/processing/vertigo-data';
+import { DataType } from 'src/app/processing/datatype';
+import { Dataspec } from 'src/app/processing/dataspec';
 
 @Component({
   selector: 'app-trampoline',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trampoline.component.scss']
 })
 export class TrampolineComponent implements OnInit {
-
-  constructor() { }
+  @Input() Progress = 100.0;
+  public VertigoRawData: VertigoRawData;
+  public VertigoProcessedData: VertigoProcessedData;
+  public GraphData: any;
+  public SelectedValues: any = { columns: [] }
+  private Graph1Series: DataType[] = Dataspec.slice(1,6,3);
+  private Graph2Series: DataType[] = Dataspec.slice(1,3,3);
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
+  public onLoaded(event: VertigoRawData) {
+    this.VertigoRawData = event;
+  }
+
+  public onProcessedLoaded(event: VertigoProcessedData) {
+    this.VertigoProcessedData = event;
+  }
+
+  public loading(event: number) {
+    this.Progress = Math.round(event * 100.0);
+  }
 }
