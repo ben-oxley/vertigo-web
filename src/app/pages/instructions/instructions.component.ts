6731e1c2ce2353434ceba3b1056b6899d8bb1a87
Ben Oxley <ben.oxley@gmail.com>
2020-01-19 13:48:14 +0000

    Updated formatting and linking

--- src/app/pages/instructions/instructions.component.ts
+++ src/app/pages/instructions/instructions.component.ts
@@ -12,4 +12,9 @@ export class InstructionsComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
 }
