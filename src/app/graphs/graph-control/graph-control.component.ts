6d1b6f49cb4d6b347662dea69f9878675c3f0335
Ben Oxley <ben.oxley@gmail.com>
2020-02-23 21:27:21 +0000

    Progress towards arbitrary processing of data series

--- src/app/graphs/graph-control/graph-control.component.ts
+++ src/app/graphs/graph-control/graph-control.component.ts
@@ -6,6 +6,10 @@ import { Data } from '../../processing/data';
 import { Dataspec } from '../../processing/dataspec';
 import { RawData } from 'src/app/processing/processes/rawdata';
 import { DataListener } from 'src/app/processing/listener';
+import { VertigoDataStoreManager } from 'src/app/processing/VertigoDataStoreManager';
+import { VertigoDataStore } from 'src/app/processing/VertigoDataStore';
+import { AbstractDataBlock } from 'src/app/processing/processes/abstractdatablock';
+import { DataBlock } from 'src/app/processing/datablock';
 
 @Component({
   selector: 'graph-control',
@@ -14,36 +18,29 @@ import { DataListener } from 'src/app/processing/listener';
 export class GraphControlComponent {
 
   public GraphData: any;
-  private vertigoRawData: VertigoRawData;
-  private vertigoProcessedData: VertigoProcessedData;
+  public DataStore: VertigoDataStore;
   private selectedSeries: DataType[] = [];
+  private renderedSeries: DataBlock[] = [];
   private renderRequested: boolean = false;
   private lastRender: number = 0;
-  private controller:GraphControlComponent;
-  private dataListener: DataListener = (a,r)=>{
-    let timeDiff:number = Math.round((Date.now() - this.lastRender) / 1000);
-    if (!this.renderRequested && (!this.lastRender || timeDiff>1)){
+  private controller: GraphControlComponent;
+  private dataListener: DataListener = (a, r) => {
+    let timeDiff: number = Math.round((Date.now() - this.lastRender) / 1000);
+    if (!this.renderRequested && (!this.lastRender || timeDiff > 1)) {
       this.renderRequested = true;
       this.lastRender = Date.now();
       this.zone.run(() => {
         this.controller.seriesChanged(this.selectedSeries);
       });
     }
-    
+
   };
 
   public constructor(private zone: NgZone) {
     this.controller = this;
     this.GraphData = [{
     }];
-  }
-
-  @Input() set RawData(vertigoRawData: VertigoRawData) {
-    this.onLoaded(vertigoRawData);
-  }
-
-  @Input() set ProcessedData(vertigoProcessedData: VertigoProcessedData) {
-    this.onProcessedLoaded(vertigoProcessedData);
+    this.DataStore = VertigoDataStoreManager.GetDataStore();
   }
 
   @Input() set SelectedSeries(selectedSeries: DataType[]) {
@@ -51,52 +48,39 @@ export class GraphControlComponent {
     this.seriesChanged(selectedSeries);
   }
 
+
+  public rerender() {
+    this.renderedSeries.forEach(r=>r.RemoveListener(this.dataListener));
+    this.renderedSeries = [];
+    this.GraphData = this.flatMap(this.selectedSeries, dt => {
+      if (this.DataStore.Get(dt)) {
+        const dataBlock: DataBlock = this.DataStore.Get(dt);
+        const data: Data[] = dataBlock.Data();
+        this.renderedSeries.push(dataBlock);
+        dataBlock.AddListener(this.dataListener);
+        if (data && data.length > 0) {
+          return dt.Columns.map(c => {
+            const t0 = data[0].Data[0];
+            const d: Date = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            d.setUTCSeconds(t0 / 1000.0);
+            const isDate = d < new Date(Date.now()) && d > new Date(0)
+            return {
+              x: data.map(datum => isDate ? new Date(datum.Data[0]).toISOString() : (datum.Data[0] - t0) / 1000.0),
+              y: data.map(datum => datum.Data[c.Identifier]),
+              name: c.Name + ' (' + c.Units + ')',
+              yaxis: c.Id,
+              typeid: dt.Identifier,
+              colid: c.Id,
+            } as any;
+          });
+        }
+      }
+    });
+  }
+
   public seriesChanged(event: DataType[]) {
-    if (this.vertigoProcessedData){
-      this.GraphData = this.flatMap(event, dt => {
-        if (this.vertigoProcessedData.DataTypes && this.vertigoProcessedData.DataTypes.has(dt.Identifier)) {
-          const data: Data[] = RawData.Cast(this.vertigoProcessedData.DataTypes.get(dt.Identifier)).Data();
-          if (data && data.length > 0) {
-            return dt.Columns.map(c => {
-              const t0 = data[0].Data[0];
-              const d: Date = new Date(0); // The 0 there is the key, which sets the date to the epoch
-              d.setUTCSeconds(t0/1000.0);
-              const isDate = d < new Date(Date.now()) && d > new Date(0) 
-              return {
-                x: data.map(datum =>isDate? new Date(datum.Data[0]).toISOString(): (datum.Data[0] - t0) / 1000.0),
-                y: data.map(datum => datum.Data[c.Identifier]),
-                name: c.Name +' ('+c.Units+')',
-                yaxis: c.Id,
-                typeid: dt.Identifier,
-                colid: c.Id,
-              } as any;
-            });
-          }
-        }
-      });
-    }
-    else if (this.vertigoRawData){
-      this.GraphData = this.flatMap(event, dt => {
-        if (this.vertigoRawData.DataTypes && this.vertigoRawData.DataTypes.has(dt.Identifier)) {
-          const data: Data[] = RawData.Cast(this.vertigoRawData.DataTypes.get(dt.Identifier)).Data();
-          if (data && data.length > 0) {
-            return dt.Columns.map(c => {
-              const t0 = data[0].Data[0];
-              const d: Date = new Date(0); // The 0 there is the key, which sets the date to the epoch
-              d.setUTCSeconds(t0/1000.0);
-              const isDate = d < new Date(Date.now()) && d > new Date(0) 
-              return {
-                x: data.map(datum =>isDate? new Date(datum.Data[0]).toISOString(): (datum.Data[0] - t0) / 1000.0),
-                y: data.map(datum => datum.Data[c.Identifier]),
-                name: c.Name +' ('+c.Units+')',
-                yaxis: c.Id,
-                typeid: dt.Identifier,
-                colid: c.Id,
-              } as any;
-            });
-          }
-        }
-      });
+    if (this.DataStore.GetAvailableDataTypes().length > 0) {
+      this.rerender();
     }
     return;
   }
@@ -109,24 +93,7 @@ export class GraphControlComponent {
 
 
   public onLoaded(event: VertigoRawData) {
-    if (this.vertigoRawData) this.vertigoRawData.DataTypes.forEach(dt=>dt.RemoveListener(this.dataListener));
-    this.vertigoRawData = event;
-    if (this.vertigoRawData) {
-      this.vertigoRawData.DataTypes.forEach(dt=>dt.AddListener(this.dataListener));
-      this.vertigoRawData.DataTypes.forEach((dt, key) => {
-        dt.AddListener((a, r) => {
-          const type = this.selectedSeries.find(s => s.Identifier === key);
-          if (type){
-            type.Columns.forEach(col => {
-              let series = this.GraphData.find(series => series.typeid === key);
-              const t0 = series.x[0] * 1000.0;
-              series.x.push(a.map(datum => (datum.Data[0] - t0) / 1000.0));
-              series.y.push(a.map(datum => datum.Data[col.Identifier]));
-            });
-          }
-        });
-      });
-    }
+    this.rerender();
 
     if (this.flatMap(this.selectedSeries, dt => dt.Columns).length === 0) {
       const spec: Dataspec = new Dataspec();
@@ -138,35 +105,7 @@ export class GraphControlComponent {
     }
   }
 
-  public onProcessedLoaded(event: VertigoProcessedData) {
-    this.vertigoProcessedData = event;
-    if (this.vertigoRawData) {
-      this.vertigoRawData.DataTypes.forEach((dt, key) => {
-        dt.AddListener((a, r) => {
-          const type = this.selectedSeries.find(s => s.Identifier === key);
-          if (type){
-            type.Columns.forEach(col => {
-              let series = this.GraphData.find(series => series.typeid === key);
-              const t0 = series.x[0] * 1000.0;
-              series.x.push(a.map(datum => (datum.Data[0] - t0) / 1000.0));
-              series.y.push(a.map(datum => datum.Data[col.Identifier]));
-            });
-          }
-        });
-      });
-    }
-
-    if (this.flatMap(this.selectedSeries, dt => dt.Columns).length === 0) {
-      const spec: Dataspec = new Dataspec();
-      spec.Types[1].Columns = spec.Types[1].Columns.slice(0, 3);
-      spec.Types = [spec.Types[1]];
-      this.seriesChanged(spec.Types);
-    } else {
-      this.seriesChanged(this.selectedSeries);
-    }
-  }
-
-  onRender(){
+  onRender() {
     this.renderRequested = false;
 
   }
